50af91eef8d7c41f08d00eb76f6e8254403fda16
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2023-06-11 11:56:54 -0300

    teste de mapa

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -1,5 +1,5 @@
 //React 
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { View, Button, Text,
         KeyboardAvoidingView, TouchableWithoutFeedback, ScrollView, Keyboard } from "react-native";
 
@@ -13,11 +13,37 @@ import AuthContext from "../../contexts/auth";
 
 
 import MapView from 'react-native-maps'
+import * as Location from 'expo-location';
 
 
 const Maps = ({ navigation }) => {
     const {} = useContext(AuthContext)
 
+    const [location, setLocation] = useState(null);
+    
+    const handleGetLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            setErrorMsg('Permissão para acessar a localização foi negada');
+            return;
+        }
+
+        try {
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        } catch (error) {
+            setErrorMsg('Erro ao obter a localização');
+            console.log(error);
+        }
+    };
+
+
+    function exibir(){
+        console.log(location)
+        //console.log("Latitude: ", location.coords.latitude)
+        //console.log("Longitude: ", location.coords.longitude)
+    }
+    
     return(
     <ScrollView contentContainerStyle={GS.ScrollContainer} >
     <KeyboardAvoidingView
@@ -35,7 +61,8 @@ const Maps = ({ navigation }) => {
                 longitudeDelta: 0.0421,
             }}
         />
-        <Button title="Obter Localização" />
+        <Button title="Obter Localização" onPress={handleGetLocation} />
+        <Button title="Exibir" onPress={() => exibir()} />
     </>
     </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
