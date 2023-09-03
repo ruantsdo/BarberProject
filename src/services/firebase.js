a86f3e1581aae43a2cb9a4460913cdd6ab980034
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2023-09-03 09:15:22 -0300

    readme added

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from "firebase/storage"
 
+/* Replace with your firebase credentials */
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ0ZrkIZuHN7V7kLuyndpswqEEDxEA4C0",
-  authDomain: "barberproject-ed1d6.firebaseapp.com",
-  projectId: "barberproject-ed1d6",
-  storageBucket: "barberproject-ed1d6.appspot.com",
-  messagingSenderId: "907197166878",
-  appId: "1:907197166878:web:93fa2abbe4eeda5885850f"
+  apiKey: "################",
+  authDomain: "################",
+  projectId: "################",
+  storageBucket: "################",
+  messagingSenderId: "################",
+  appId: "################"
 };
 
 const app = initializeApp(firebaseConfig)
